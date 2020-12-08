5d2d23462a478f56d73629e7851080760fe3b3df
jamesr1775 <jamesr1775@yahoo.co.uk>
2020-12-08 15:32:14 +0000

    rm files

--- python_solutions/TestIsUnique.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from IsUnique import IsUnique
-
-class TestIsUnique(unittest.TestCase):
-    def test_case_1(self):
-        self.assertEqual(IsUnique("abc"), True)
-    def test_case_2(self):
-        self.assertEqual(IsUnique("abcdd"), False)
-
